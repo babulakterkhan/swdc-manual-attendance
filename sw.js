a8ff2cfc0b3ccf7b14e2099d27b81bf1454a00ed
babulakterkhan <108738806+babulakterkhan@users.noreply.github.com>
2026-01-02 01:18:47 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const cacheName = 'attendance-v1';
+const staticAssets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
